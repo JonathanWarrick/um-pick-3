6dc6f9952ba9f5d32bd12a7128c1623d5b51f3fb
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-28 19:02:59 -0500

    Add Submission model (with no associations or fields right now).

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,10 @@
 class SubmissionsController < ApplicationController
 	def new
-		
+
+	end
+
+	def create	
+		# shouldn't render view; for now, let's just render the stuff
+		render plain: params[:submission].inspect
 	end
 end
