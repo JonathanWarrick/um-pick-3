500d1dff66b499700bee6718770cd1991f87be16
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-28 19:06:59 -0500

    Add User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129000139) do
+ActiveRecord::Schema.define(version: 20150129000645) do
 
   create_table "submissions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
