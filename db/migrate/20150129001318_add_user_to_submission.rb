249e6e6c0ab7e0e17950c547ba992411bbbcb3be
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-28 19:14:38 -0500

    Migration to associate submissions and users.

---
+++ db/migrate/20150129001318_add_user_to_submission.rb
@@ -0,0 +1,6 @@
+class AddUserToSubmission < ActiveRecord::Migration
+  def change
+    add_reference :submissions, :user, index: true
+    add_foreign_key :submissions, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129000928) do
+ActiveRecord::Schema.define(version: 20150129001318) do
 
   create_table "guesses", force: :cascade do |t|
     t.integer  "submission_id"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20150129000928) do
   create_table "submissions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "submissions", ["user_id"], name: "index_submissions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
