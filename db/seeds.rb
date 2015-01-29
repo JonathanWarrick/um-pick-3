6774dfae3abb1bc86f7f55c0bfd2f02b5620cc02
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-29 01:22:03 -0500

    Re-seed information!

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,40 @@
 Song.create(song_name: 'Resolution')
 Song.create(song_name: 'Believe The Lie')
 Song.create(song_name: 'Anchor Drops')
+Song.create(song_name: 'Hurt Bird Bath')
+Song.create(song_name: "Miss Tinkle's Overture")
+Song.create(song_name: "Mulche's Odyssey")
+Song.create(song_name: "1348")
+Song.create(song_name: "#5")
+Song.create(song_name: "In The Kitchen")
+Song.create(song_name: "Domino Theory")
 
 # Populate Users List
 User.create(name: 'Jonathan Warrick', email: 'Jonathan@Test.com')
 User.create(name: 'Danny Warrick', email: 'Danny@Test.com')
+User.create(name: 'Jim Warrick', email: 'Jim@Test.com')
+User.create(name: 'Carole Warrick', email: 'Carole@Test.com')
+
+# Populate Shows List
+Show.create(date_of_show: Time.zone.now - 1.day, location_of_show: "New York, NY")
+Show.create(date_of_show: Time.zone.now - 4.day, location_of_show: "Baltimore, MD")
+Show.create(date_of_show: Time.zone.now - 1.month, location_of_show: "Los Angeles, CA")
+
+# Populate Song_Shows List
+Show.first.song_shows.create(song_id: Song.find_by(song_name: 'Resolution').id)
+Show.first.song_shows.create(song_id: Song.find_by(song_name: 'Hurt Bird Bath').id)
+Show.first.song_shows.create(song_id: Song.find_by(song_name: 'In The Kitchen').id)
+Show.first.song_shows.create(song_id: Song.find_by(song_name: '#5').id)
 
 # Populate Submissions List
 User.first.submissions.create
 User.last.submissions.create
 
 # Populate Guesses List
-Submission.first.guesses.create
-Submission.last.guesses.create
+Submission.first.guesses.create(song_id: Song.find_by(song_name: 'Hurt Bird Bath').id)
+Submission.first.guesses.create(song_id: Song.find_by(song_name: 'Believe The Lie').id)
+Submission.first.guesses.create(song_id: Song.find_by(song_name: 'Anchor Drops').id)
+
+Submission.last.guesses.create(song_id: Song.find_by(song_name: 'Resolution').id)
+Submission.last.guesses.create(song_id: Song.find_by(song_name: 'In The Kitchen').id)
+Submission.last.guesses.create(song_id: Song.find_by(song_name: '1348').id)
