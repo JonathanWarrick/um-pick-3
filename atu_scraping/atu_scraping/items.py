3077621056581334bf7b0b43cd86315373367305
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-29 23:00:49 -0500

    Outline for Scrapy project to do scraping of ATU.

---
+++ atu_scraping/atu_scraping/items.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class AtuScrapingItem(scrapy.Item):
+  # UM catalog
+  song_name = scrapy.Field()
+  song_artist = scrapy.Field()
+  times_played = scrapy.Field()
+  debut_date = scrapy.Field()
+  last_played_date = scrapy.Field()
+
+  # UM show
+  show_date = scrapy.Field()
+  venue = scrapy.Field()
+  city = scrapy.Field()
+  state = scrapy.Field()
+  country = scrapy.Field()
+  song_played = scrapy.Field()
