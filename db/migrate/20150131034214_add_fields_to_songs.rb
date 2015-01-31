7149612c65dfb8a9d6a1c4ec8570ba28ac52b16a
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-30 22:43:19 -0500

    Ignore seed data for the time being. Update Song table.

---
+++ db/migrate/20150131034214_add_fields_to_songs.rb
@@ -0,0 +1,8 @@
+class AddFieldsToSongs < ActiveRecord::Migration
+  def change
+    add_column :songs, :song_artist, :string
+    add_column :songs, :times_played, :integer
+    add_column :songs, :debut_date, :datetime
+    add_column :songs, :last_played_dated, :datetime
+  end
+end
