81aafdb2d3db0764d55cbf378302c33bcbc60d73
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-02-04 00:46:41 -0500

    Why is CSS so annoying?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# Returns default page title if none is provided
+	def provide_title(page_title = '')
+		base_title = "Umphrey's Pick 3"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	def setup
+		@base_title = "Umphrey's Pick 3"
+	end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Umphrey's Pick 3 | Home"
+    assert_select "title", @base_title
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
-  	assert_select "title", "Umphrey's Pick 3 | Contact"
+  	assert_select "title", "#{@base_title} | Contact"
   end
 
 end
