d0293455ca821ed0ade7516c318082a957562254
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-02-24 20:33:40 -0500

    Add pieces of remembering (need to add interface/UI - 8.4.5).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205044631) do
+ActiveRecord::Schema.define(version: 20150225012354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150205044631) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
