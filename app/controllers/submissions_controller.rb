e3be25d00f3b4bdc1f325d44b32f73e67de91a72
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-03-02 17:25:47 -0500

    Populate submission information.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,6 +1,8 @@
 class SubmissionsController < ApplicationController
 	def new
-
+		@um_songs = Song.where(:song_artist => ["Umphrey's McGee", "Ali Baba's Tahini", "Karl Engelmann"]).map {|x| x.song_name}
+		@cover_songs = Song.where.not(:song_artist => ["Umphrey's McGee", "Ali Baba's Tahini", "Karl Engelmann"]).map {|x| "#{x.song_name} by #{x.song_artist}"}
+		@upcoming_shows = Show.where(:is_graded => false).map {|x| "#{x.date_of_show}: #{x.show_city}, #{x.show_state}"}
 	end
 
 	def create	
