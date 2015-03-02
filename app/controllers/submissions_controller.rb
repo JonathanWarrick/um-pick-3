bf3df99414e780e27d1fe2d9054794eb2bc1b8d6
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-03-02 18:04:25 -0500

    Successfully saves new submissions (without validation) to database.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,12 +1,21 @@
 class SubmissionsController < ApplicationController
-	def new
+	def new		
 		@um_songs = Song.where(:song_artist => ["Umphrey's McGee", "Ali Baba's Tahini", "Karl Engelmann"]).map {|x| x.song_name}
-		@cover_songs = Song.where.not(:song_artist => ["Umphrey's McGee", "Ali Baba's Tahini", "Karl Engelmann"]).map {|x| "#{x.song_name} by #{x.song_artist}"}
-		@upcoming_shows = Show.where(:is_graded => false).map {|x| "#{x.date_of_show}: #{x.show_city}, #{x.show_state}"}
+		@cover_songs = Song.where.not(:song_artist => ["Umphrey's McGee", "Ali Baba's Tahini", "Karl Engelmann"]).map {|x| x.song_name}
+		@upcoming_shows = Show.where(:is_graded => false).map {|x| x.date_of_show}
 	end
 
 	def create	
 		# shouldn't render view; for now, let's just render the stuff
 		render plain: params[:submission].inspect
+		puts params
+		puts "HEYYYYYYYY #{@submission}"
+		@user ||= User.first
+		# binding.pry
+		@submission = @user.submissions.create({:show_id => Show.find_by_date_of_show(params[:submission][:date_of_show]).id})
+		@submission.guesses.create({:song_id => Song.find_by_song_name(params[:submission][:guess1]).id, :is_opener => true})
+		@submission.guesses.create({:song_id => Song.find_by_song_name(params[:submission][:guess2]).id})
+		@submission.guesses.create({:song_id => Song.find_by_song_name(params[:submission][:guess3]).id})
+		@submission.guesses.create({:song_id => Song.find_by_song_name(params[:submission][:cover]).id, :is_cover => true})
 	end
 end
